3b5da5df59960337b3ab13a62c19a279ed96e862
Bagouda <rubread56@gmail.com>
2022-06-24 10:05:56 +0200

    routage 0.1

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,29 @@ var router = express.Router();
 
 var database = require('../main');
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "sql11.freesqldatabase.com",
+    user: "sql11501090",
+    password: "8ZZez17pGn",
+    database: "sql11501090",
+    port: "3306"
+});
+sql = "Select t.type_nom from type_ingredient t"
+con.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected!");
+
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+
+});
+
+module.exports = con;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +36,6 @@ router.get('/', function(req, res, next) {
 router.post('/login', function(req, res, next){
     res.send('Test')
     res.end();
-})
+});
 
 module.exports = router;
